1c8b0188e422a9c4d367950093bcccbb79f76b7f
Kartheek Kotha <kartheekkotha20041@gmail.com>
2023-12-18 14:35:41 +0530

    Completedd the agent for the game , left to train the model and add RL

--- agent.py
+++ agent.py
@@ -28,20 +28,80 @@ class Agent:
         self.epsilon = 0# randomness
         self.gamma =0 # discount rate
         self.memory = deque(maxlen= MAX_MEMORY)# popleft()
+        self.model = None   #TODO
+        self.trainer = None #TODO       
         #   TODO:   model , trainer
 
     def get_state(self, game):
-        pass
+        #[isDangerStraight , isDangerRight , isDangerLeft , isDirectionLeft , isDirectionRight , isDirectionUp , isDirectionDown 
+        #, isFoodLeft , isFoodRight , isFoodUp , isFoodDown]
+        head = game.snake[0]
+        point_l = Point(head.x -20 , head.y)
+        point_r = Point(head.x +20 , head.y)
+        point_u = Point(head.x , head.y -20)
+        point_d = Point(head.x , head.y +20)
+
+        dir_l = game.direction == Direction.LEFT
+        dir_r = game.direction == Direction.RIGHT
+        dir_u = game.direction == Direction.UP
+        dir_d = game.direction == Direction.DOWN
+
+        state = [
+            # Danger straight
+            (dir_r and game.is_collision(point_r)) or
+            (dir_l and game.is_collision(point_l)) or
+            (dir_u and game.is_collision(point_u)) or
+            (dir_d and game.is_collision(point_d)),
+            # Danger right
+            (dir_u and game.is_collision(point_r)) or
+            (dir_d and game.is_collision(point_l)) or
+            (dir_l and game.is_collision(point_u)) or
+            (dir_r and game.is_collision(point_d)),
+            # Danger left
+            (dir_d and game.is_collision(point_r)) or
+            (dir_u and game.is_collision(point_l)) or   
+            (dir_r and game.is_collision(point_u)) or
+            (dir_l and game.is_collision(point_d)),
+            # Move direction
+            dir_l ,
+            dir_r ,
+            dir_u ,
+            dir_d ,
+            # Food location
+            game.food.x < game.head.x , # food left
+            game.food.x > game.head.x , # food right
+            game.food.y < game.head.y , # food up
+            game.food.y > game.head.y , # food down
+        ]
+        return np.array(state , dtype=int)
     def remember(self , state , action , reward , next_state , isDone):
-        pass
+        self.memory.append((state , action , reward , next_state , isDone))
 
     def train_long_memory(self):
-        pass
+        if len(self.memory) > BATCH_SIZE:
+            mini_sample = random.sample(self.memory , BATCH_SIZE)
+        else:
+            mini_sample = self.memory
+        states , actions , rewards , next_states , isDones = zip(*mini_sample)
+        self.trainer.train_step(states , actions , rewards , next_states , isDones)
+
     def train_short_memory(self, state , action , reward , next_state , isDone):
-        pass
+        self.trainer.train_step(state , action , reward , next_state , isDone)
+
 
     def get_action(self , state):
-        pass
+        # random moves : tradeoff exploration / exploitation
+        self.epsilon = 80 - self.n_games
+        final_move = [0,0,0]
+        if random.randint(0,200) < self.epsilon:
+            move = random.randint(0,2)
+            final_move[move] = 1
+        else:
+            state0 = torch.tensor(state , dtype=torch.float)
+            prediction = self.model.predict(state0)
+            move = torch.argmax(prediction).item()
+            final_move[move] = 1
+        return final_move
 
 
 def train():
